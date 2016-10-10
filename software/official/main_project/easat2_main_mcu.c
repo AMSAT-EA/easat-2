fc2599161f331470e375589404b0245d3580876b
Felix Paez EA4GQS <ea4gqs@gmail.com>
2016-10-10 21:43:51 +0200

    Code folders reorganization and configuration version in C
    Code folders reorganization and configuration version in C

---
+++ software/official/main_project/easat2_main_mcu.c
@@ -0,0 +1,17 @@
+/*
+ *                                                       
+ * Project     : EASAT2                                            
+ * File        : easat2_main_mcu.c
+ *
+ * Description : EASAT2 MCU main program
+ * Last update : 10 October 2016                                              
+ *                                                                            
+*/
+
+#include"easat2_inc_config.h"
+        
+    void main(void) {
+    
+        return;
+    
+}
