8d48ea64264d51a2f3a5adebfcefba559433060a
Felix Paez EA4GQS <ea4gqs@gmail.com>
2017-06-01 21:10:42 +0200

    ASK decoder first version - untested
    ASK decoder main code, first version - untested

--- software/official/main_project/easat2_func_commands.h
+++ software/official/main_project/easat2_func_commands.h
@@ -13,6 +13,13 @@
 
 #include "easat2.h"
 
+#define ASK_COMMAND_LENGTH                    32 // command length in bits
 // functions prototypes
+
+// global variables
+
+unsigned int global_command_buffer; 			// 32 bits = 8 bits 0x55 header + 24 golay bits //
+unsigned int global_command_pending_check;               // flag that indicates if a command is pending decoding
+
 
 #endif
